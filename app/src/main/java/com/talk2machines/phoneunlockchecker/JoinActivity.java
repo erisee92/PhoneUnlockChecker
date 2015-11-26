09f6770f8138fb5c1a39d802e5f06fa2254833c3
shengnull <lin_null@yahoo.de>
2015-11-26 16:08:16 +0100

    Funktionalität von main- und  login- activity

--- app/src/main/java/com/talk2machines/phoneunlockchecker/JoinActivity.java
+++ app/src/main/java/com/talk2machines/phoneunlockchecker/JoinActivity.java
@@ -1,7 +1,14 @@
 package com.talk2machines.phoneunlockchecker;
 
-/**
- * Created by lin_n on 19.11.2015.
- */
-public class JoinActivity {
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JoinActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
 }
